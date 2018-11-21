d576379c119183abb2a0af03e762faedf14349bd
naveen <aryan.b509@gmail.com>
2018-11-21 21:23:38 +0530

    First commit Message

---
+++ src/main/java/com/rest/example/gaganapplication/GaganapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.rest.example.gaganapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaganapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaganapplicationApplication.class, args);
+		System.out.println("hello world !");
+		
+	}
+}
